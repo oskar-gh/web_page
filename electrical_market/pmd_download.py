9e33cbe477fa509ef30669df3b733c0639248f2c
oskar-gh <oscarhb00@hotmail.com>
2024-09-18 21:23:32 +0200

    adaptation on the PMD prices website

--- electrical_market/pmd_download.py
+++ electrical_market/pmd_download.py
@@ -1,27 +1,39 @@
 import requests
 import pandas as pd
+import sys
+import os
+
+# Add the project root directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from datetime import datetime, timedelta
+from utils.utils import create_csv_inlocal, create_csv_inbuffer
+from ree_request import get_data_from_api
 
-# Función para obtener datos de la API
-def get_data_from_api(url, api_key):
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-api-key': api_key  # Incluye la clave API en el encabezado
-    }
-    
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
+def pmd_download():
+    # Get tomorrow's date
+    if datetime.now().hour >= 15:
+        target_date = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
     else:
-        print(f"Error {response.status_code}: {response.text}")
-        return None
+        target_date = (datetime.now()).strftime('%Y-%m-%d')
+    
+    # URL for daily market price indicator (OMIE_PMD - indicator 600)
+    omiepmd_url = f"https://api.esios.ree.es/indicators/600?start_date={target_date}T00:00&end_date={target_date}T23:59"
 
-# Procesar los datos en un DataFrame
+    # Obtain and process daily market data
+    omiepmd_data = get_data_from_api(omiepmd_url)
+    omiepmd_df = process_data(omiepmd_data)
+
+    if omiepmd_df is not None:
+        #create_csv_inlocal(omiepmd_df,"C:/Users/hp/Documents/Óscar","precios_omiepmd.csv")
+        csv_buffer = create_csv_inbuffer(omiepmd_df)
+        return csv
+        print("File 'prices_omiepmd.csv' created.")
+
+# Process data in a DataFrame
 def process_data(data):
     if data:
         try:
-            # Mapeo de geo_id a nombre del país
+            # Mapping geo_id to country name
             geo_mapping = {
                 1: "Portugal",
                 2: "Francia",
@@ -33,14 +45,13 @@ def process_data(data):
                 8828: "Países Bajos"
             }
 
-            # Ajusta esta línea según el formato de los datos que obtienes
             values = data['indicator']['values'] if 'indicator' in data and 'values' in data['indicator'] else data
             df = pd.DataFrame(values)
             
-            # Imprimir el DataFrame para depuración
-            print("DataFrame inicial:", df.head())
+            # Print the DataFrame for debugging
+            # print("DataFrame inicial:", df.head())
 
-            # Añadir columna de país
+            # Add country column
             if 'geo_id' in df.columns and 'datetime' in df.columns and 'value' in df.columns:
                 df['datetime'] = pd.to_datetime(df['datetime'])
                 df['Fecha'] = df['datetime'].dt.date
@@ -48,48 +59,16 @@ def process_data(data):
                 df['Precio'] = df['value'].astype(float)
                 df['País'] = df['geo_id'].map(geo_mapping)
                 
-                # Pivotar el DataFrame
+                # Pivot the DataFrame
                 df_pivot = df.pivot_table(index=['Fecha', 'Hora'], columns='País', values='Precio')
                 df_pivot.reset_index(inplace=True)
                 
                 return df_pivot
             else:
-                print("Columnas esperadas no encontradas en los datos")
+                print("Expected columns not found in data")
                 return None
         except Exception as e:
-            print(f"Error al procesar los datos: {e}")
+            print(f"Error processing data: {e}")
             return None
     else:
         return None
-
-# Crear archivos CSV
-def create_csv(df, filename):
-    try:
-        # Especifica la ruta completa si deseas guardar en un directorio específico
-        full_path = f"C:/Users/hp/Documents/Óscar/{filename}"
-        df.to_csv(full_path, index=False, encoding='utf-8')
-        print(f"Archivo CSV guardado en: {full_path}")
-    except Exception as e:
-        print(f"Error al crear el archivo CSV: {e}")
-
-def main():
-    # Clave API (asegúrate de sustituirla por la tuya)
-    api_key = "fbfd13345575ac521663e367df30483d271e0a8cab8fc83ff0d61607754ddf17" 
-    
-    # Obtener la fecha de mañana
-    #tomorrow = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
-    tomorrow = (datetime.now()).strftime('%Y-%m-%d')
-
-    # URL para el indicador de precios del mercado diario (OMIE_PMD - indicador 600)
-    omiepmd_url = f"https://api.esios.ree.es/indicators/600?start_date={tomorrow}T00:00&end_date={tomorrow}T23:59"
-
-    # Obtener y procesar datos del mercado diario (OMIE_PMD)
-    omiepmd_data = get_data_from_api(omiepmd_url, api_key)
-    omiepmd_df = process_data(omiepmd_data)
-
-    if omiepmd_df is not None:
-        create_csv(omiepmd_df, "precios_omiepmd.csv")
-        print("Archivo precios_omiepmd.csv creado.")
-
-if __name__ == "__main__":
-    main()
