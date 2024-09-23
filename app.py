e940776d8a275309175d1649bb0fd71285ff4fa2
oskar-gh <oscarhb00@hotmail.com>
2024-09-24 00:15:47 +0200

    test

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 from electrical_market import pmd_download
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/css', static_folder='css')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/coins')
+def index():
+    return render_template('coins_index.html')
+
+@app.route('/cv_spanish')
+def index():
+    return render_template('cv_index_spanish.html')
+
+@app.route('/cv_english')
+def index():
+    return render_template('cv_index_english.html')
+
+@app.route('/family')
+def index():
+    return render_template('family_index.html')
 
 @app.route('/electrical_market')
 def electrical_market():
