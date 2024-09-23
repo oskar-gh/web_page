0061c4f3a15777a8dd16d80003b4e31c90fc3918
oskar-gh <oscarhb00@hotmail.com>
2024-09-24 00:34:14 +0200

    test2

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__, static_url_path='/css', static_folder='css')
 def index():
     return render_template('index.html')
 
-@app.route('/coins')
+@app.route('/coins_index')
 def coins_index():
     return render_template('coins_index.html')
 
-@app.route('/cv_spanish')
+@app.route('/cv_index_spanish')
 def cv_index_spanish():
     return render_template('cv_index_spanish.html')
 
-@app.route('/cv_english')
+@app.route('/cv_index_english')
 def cv_index_english():
     return render_template('cv_index_english.html')
 
-@app.route('/family')
+@app.route('/family_index')
 def family_index():
     return render_template('family_index.html')
 
-@app.route('/electrical_market')
+@app.route('/electrical_market_index')
 def electrical_market_index():
     df = pmd_download.pmd_download()
     return render_template('electrical_market_index.html', prices=df.to_html(classes='data'))
--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // script.js
 
 // Seleccionar todos los elementos con submenú
-const toggleMenus = document.querySelectorAll('.toggle-menu');
+//const toggleMenus = document.querySelectorAll('.toggle-menu');
 
-toggleMenus.forEach(menu => {
-    menu.addEventListener('click', function(e) {
-        // Si el elemento tiene un submenú, entonces evitamos el comportamiento predeterminado (ir a un enlace)
-        const submenu = this.nextElementSibling;
-        if (submenu && submenu.classList.contains('submenu')) {
-            e.preventDefault();
-            // Alternar la clase 'open' en el padre para mostrar/ocultar el submenú
-            this.parentElement.classList.toggle('open');
-        }
+//toggleMenus.forEach(menu => {
+//    menu.addEventListener('click', function(e) {
+//        // Si el elemento tiene un submenú, entonces evitamos el comportamiento predeterminado (ir a un enlace)
+//        const submenu = this.nextElementSibling;
+//        if (submenu && submenu.classList.contains('submenu')) {
+//            e.preventDefault();
+//            // Alternar la clase 'open' en el padre para mostrar/ocultar el submenú
+//            this.parentElement.classList.toggle('open');
+//        }
+//    });
+//});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleMenuItems = document.querySelectorAll('.toggle-menu');
+
+    toggleMenuItems.forEach(item => {
+        item.addEventListener('click', function(e) {
+            const submenu = item.nextElementSibling; // Encuentra el siguiente <ul> (submenú)
+            if (submenu) {
+                submenu.classList.toggle('submenu'); // Alternar la visibilidad del submenú
+            }
+            e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+        });
     });
 });
